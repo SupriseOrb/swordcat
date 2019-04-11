5eed10420ab564ec5345555ce5291e7698f0129f
Xavier Chua <xavierc.1998@gmail.com>
2019-04-11 16:58:46 -0700

    Added personal scene, basic player movement script

---
+++ Sword Cat Unity/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] float m_moveSpeed;
+    [SerializeField] Vector3 m_direction;
+
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = this.transform.GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //Debug.Log(m_direction.normalized * m_moveSpeed);
+        rb.velocity = m_direction.normalized * m_moveSpeed;
+    }
+}
