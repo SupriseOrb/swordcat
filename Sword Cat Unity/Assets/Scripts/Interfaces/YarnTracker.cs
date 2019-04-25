1dc950fe39bdf88c63c9385b692a7e74ee27297d
Conan Truong <truong.conan@gmail.com>
2019-04-25 16:52:19 -0700

    Yarn Count UI

---
+++ Sword Cat Unity/Assets/Scripts/Interfaces/YarnTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class YarnTracker : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI redYarnAmount;
+    [SerializeField] TextMeshProUGUI greenYarnAmount;
+    [SerializeField] TextMeshProUGUI purpleYarnAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        redYarnAmount.text = GameManager.instance.data.inventory[(int) TumbleYarn.YarnType.RED].ToString();
+        greenYarnAmount.text = GameManager.instance.data.inventory[(int)TumbleYarn.YarnType.GREEN].ToString();
+        purpleYarnAmount.text = GameManager.instance.data.inventory[(int)TumbleYarn.YarnType.PURPLE].ToString();
+    }
+}
