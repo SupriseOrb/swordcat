5b342e058f8f6cd44c7d08c77f4797012de29f53
Conan Truong <truong.conan@gmail.com>
2019-05-16 16:24:41 -0700

    Add loading zone script

---
+++ Sword Cat Unity/Assets/Scripts/Gameplay/LoadingZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingZone : MonoBehaviour
+{
+    [SerializeField] string newSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadSceneAsync(newSceneName);
+        }
+
+        yield break;
+    }
+}
